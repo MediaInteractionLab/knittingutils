7905b3bd38529f6f6fe74034cc587f4778b632b7
Roland Aigner <roland.aigner@fh-hagenberg.at>
2021-10-12 15:47:57 +0200

    added more Sintral samples

---
+++ samples/intarsiaSintral.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+/*
+v2
+*/
+
+"use strict";
+
+let outFileName = 'out.k';
+if(process.argv[2]) {
+    outFileName = process.argv[2];
+    let lcfn = outFileName.toLowerCase();
+    if(!lcfn.endsWith(".k") && !lcfn.endsWith(".knitout")) {
+        outFileName += ".k";
+        console.warn("filename must end with '.k' or '.knitout', corrected to " + outFileName);
+    }
+}
+
+function generateKnit(){
+
+    let ku = require("../knittingutils.js");
+    let ks = new ku.KnitSequence();
+
+    //create yarn descriptors for two different yarns
+    let yarnPoly0 =    ks.makeYarn("Polyester0");
+    let yarnPoly1 =    ks.makeYarn("Polyester1");
+
+    let courses = 100;
+    let wales = 50;
+
+    let intarsiaCourses = 50;
+    let intarsiaWales = 30;
+
+    let marginCourses = Math.round((courses - intarsiaCourses) / 2);
+    let marginWales = Math.round((wales - intarsiaWales) / 2);
+
+    //knit lower end with only poly0
+    ks.comment("lower margin area");
+    for(let j = 0; j < marginCourses; j++) {
+
+        //create new poly0 course
+        ks.newCourse(yarnPoly0);
+
+        //fill course with operations 'k' for front knit operation
+        // fill _wales_ needles with repeat pattern "k"
+        ks.insert(yarnPoly0, "k", wales);
+    }
+
+    ks.comment("knitting intarsia area");
+    for(let j = 0; j < intarsiaCourses; j++) {
+
+        //create new poly1 course
+        // start course at (margin-1)
+        ks.newCourse(yarnPoly1, marginWales - 1);
+        //insert a single 't' for a front tuck
+        ks.insert(yarnPoly1, "t");
+        //insert 'k' for front knit
+        // fill _intarsiaWales_ needles with repeat pattern "k"
+        ks.insert(yarnPoly1, "k", intarsiaWales);
+        //insert a single 't' for a front tuck
+        ks.insert(yarnPoly1, "t");
+
+        //create new poly0 course
+        ks.newCourse(yarnPoly0);
+        //insert 'k' for front knit
+        // fill _marginWales_ needles with repeat pattern "k"
+        ks.insert(yarnPoly0, "k", marginWales);
+        //insert '-' and 't', specify a repeat of three misses and one front tuck
+        // fill _marginWales_ needles with repeat pattern "---t"
+        // pass alternating repeat pattern offste for every course (0, 1, 2, 3, 0, 1, 2, 3...) to get somewhat balanced inlay
+        ks.insert(yarnPoly0, "---t", intarsiaWales, j % 4);
+        //insert 'k' for front knit
+        // fill _marginWales_ needles with repeat pattern "k"
+        ks.insert(yarnPoly0, "k", marginWales);
+    }
+
+    //knit lower end with only poly0
+    ks.comment("upper margin area");
+    for(let j = 0; j < marginCourses; j++) {
+
+        //create new poly0 course
+        ks.newCourse(yarnPoly0);
+
+        //fill course with operations 'k' for front knit operation
+        // fill _wales_ needles with repeat pattern "k"
+        ks.insert(yarnPoly0, "k", wales);
+    }
+
+    //shift entire pattern one needle to the right, otherwise castOff 
+    // would need to access negative needle indices, when done from
+    // right-to-left
+    ks.shift(50);
+
+    //print all maps to console
+    ks.printAllMaps();
+
+    //print command order to console
+    ks.printOrder();
+
+    //print entire interlaced knitting sequence to console
+    ks.printSequence();
+
+    //map yarn to carrier #3
+    ks.mapYarn(yarnPoly0, 3);
+    ks.mapYarn(yarnPoly1, 4);
+
+    //create knitout and write file
+    ks.generate(outFileName, "intarsia in single jersey", "Keep", undefined, true, false, true, ku.SINTRAL);
+}
+
+generateKnit();
---
+++ samples/jacquardSintral.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+
+/*
+v2
+*/
+
+
+//map code (basically, a binary image) to a knit
+let code = [
+    "                       ",
+    " xxxxxxx  x  x xxxxxxx ",
+    " x     x x  x  x     x ",
+    " x xxx x  x    x xxx x ",
+    " x xxx x x  x  x xxx x ",
+    " x xxx x   xxx x xxx x ",
+    " x     x xxx x x     x ",
+    " xxxxxxx x x x xxxxxxx ",
+    "           xxx         ",
+    " xxxxx xxxx  xx x x x  ",
+    "     xx   xx xxxxx   x ",
+    " x x x x  x x xx  xxx  ",
+    " x   x  x x   x xx xx  ",
+    "   x   x    x xx x  xx ",
+    "         xx xx  xxx  x ",
+    " xxxxxxx xx  x xx xxx  ",
+    " x     x  xxxxx xxxx x ",
+    " x xxx x x x x xx    x ",
+    " x xxx x xx  xx xxx    ",
+    " x xxx x x xx xx   x   ",
+    " x     x xx       xx   ",
+    " xxxxxxx x  x   x   x  ",
+    "                       "
+    ];
+    
+    "use strict";
+    
+    let outFileName = 'out.k';
+    if(process.argv[2]) {
+        outFileName = process.argv[2];
+        let lcfn = outFileName.toLowerCase();
+        if(!lcfn.endsWith(".k") && !lcfn.endsWith(".knitout")) {
+            outFileName += ".k";
+            console.warn("filename must end with '.k' or '.knitout', corrected to " + outFileName);
+        }
+    }
+    
+    function generateKnit(){
+    
+        let ku = require("../knittingutils.js");
+        let ks = new ku.KnitSequence();
+    
+        //create yarn descriptors
+        let yarnPolyW =    ks.makeYarn("Polyester white");
+        let yarnPolyB =    ks.makeYarn("Polyester black");
+    
+        let width = code[0].length;
+        let height = code.length;
+    
+        let cpp = 5;    //courses per pixel
+        let wpp = 3;    //wales per pixel
+    
+        let wales = width * wpp;
+        let courses = height * cpp;
+    
+        //rack back bad for front/back knitting
+        ks.rack(0.25);
+    
+        for(let j = 0; j < courses; j++) {
+    
+            //create strings, supposed to hold the patterns for both yarns, as courses cannot be written, alternatingly
+            let lineW = "";
+            let lineB = "";
+            //create birds eye jacquard pattern from code (or binary image)
+            // front bed is knit according to image content
+            // back bed must be knit alternatingly with black/white
+            for(let i = 0; i < wales; i++) {
+                if(code[Math.floor((courses - 1 - j) / cpp)][Math.floor(i / wpp)] === ' ') {
+                    lineW += (i % 2 ? 'k' : 'b');
+                    lineB += (i % 2 ? '-' : 'K');
+                } else {
+                    lineW += (i % 2 ? 'K' : '-');
+                    lineB += (i % 2 ? 'b' : 'k');
+                }
+            }
+    
+            //now write patterns to courses
+            //create new course with white yarn
+            ks.newCourse(yarnPolyW);
+            ks.insert(yarnPolyW, lineW);
+    
+            //create new course with black yarn
+            ks.newCourse(yarnPolyB);
+            ks.insert(yarnPolyB, lineB);
+        }
+    
+        //shift entire pattern one needle to the right, otherwise castOff 
+        // would need to access negative needle indices, when done from
+        // right-to-left
+        ks.shift(50);
+    
+        //print all maps to console
+        //ks.printAllMaps();
+    
+        //print command order to console
+        //ks.printOrder();
+    
+        //print entire interlaced knitting sequence to console
+        //ks.printSequence();
+    
+        //map yarns to carriers
+        ks.mapYarn(yarnPolyW, 3);
+        ks.mapYarn(yarnPolyB, 5);
+    
+        //create knitout and write file
+        ks.generate(outFileName, "QR code via birds eye jacquard", "Keep", undefined, true, false, true, ku.SINTRAL );
+    }
+    
+    generateKnit();
